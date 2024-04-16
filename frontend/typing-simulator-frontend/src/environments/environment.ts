5d12d8c38b68cb3972b86112293ea950255ae27e
Cecila <cecilachewyc@gmail.com>
2024-04-17 07:13:31 +0800

    Debug

--- frontend/typing-simulator-frontend/src/environments/environment.ts
+++ frontend/typing-simulator-frontend/src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:8080',
+    // apiUrl: 'http://localhost:8080',
+    apiUrl: '${API_URL}',
     github_client_id: 'your-github-client-id',
     redirectUri: 'http://localhost:4200/callback',
 };
