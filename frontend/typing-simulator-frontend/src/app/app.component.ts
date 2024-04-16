2045ad7c5dd00b5536351b59ba1b57e256de156f
Cecila <cecilachewyc@gmail.com>
2024-04-17 07:25:32 +0800

    Debug

--- frontend/typing-simulator-frontend/src/app/app.component.ts
+++ frontend/typing-simulator-frontend/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { LoginRegisterComponent } from './components/login-register/login-register.component';
 import { UserHistoryComponent } from './components/user-history/user-history.component';
 import { MainApplicationComponent } from './components/main-application/main-application.component';
@@ -14,30 +15,14 @@ import { LeaderboardComponent } from './components/leaderboard/leaderboard.compo
     RouterModule,
     MatToolbarModule,
     MatButtonModule,
+    MatIconModule,
     LoginRegisterComponent,
     UserHistoryComponent,
     MainApplicationComponent,
     LeaderboardComponent
   ],
-  template: `
-    <mat-toolbar color="primary">
-      <span>Typing Simulator</span>
-      <span class="spacer"></span>
-      <button mat-button routerLink="/login">Login/Register</button>
-      <button mat-button routerLink="/user-history">User History</button>
-      <button mat-button routerLink="/main-application">Main Application</button>
-      <button mat-button routerLink="/leaderboard">Leaderboard</button>
-    </mat-toolbar>
-
-    <router-outlet></router-outlet>
-  `,
-  styles: [
-    `
-      .spacer {
-        flex: 1 1 auto;
-      }
-    `,
-  ],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'typing-simulator-frontend';
