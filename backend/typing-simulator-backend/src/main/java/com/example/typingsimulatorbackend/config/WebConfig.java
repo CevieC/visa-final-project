95e8679bd20e4c03b45bb145dea6161f0d310391
Cecila <cecilachewyc@gmail.com>
2024-04-17 21:15:49 +0800

    Debug

--- backend/typing-simulator-backend/src/main/java/com/example/typingsimulatorbackend/config/WebConfig.java
+++ backend/typing-simulator-backend/src/main/java/com/example/typingsimulatorbackend/config/WebConfig.java
@@ -1,17 +1,17 @@
-// package com.example.typingsimulatorbackend.config;
+package com.example.typingsimulatorbackend.config;
 
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.web.servlet.config.annotation.CorsRegistry;
-// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-// @Configuration
-// public class WebConfig implements WebMvcConfigurer {
-//     @Override
-//     public void addCorsMappings(CorsRegistry registry) {
-//         registry.addMapping("/api/**")
-//                 .allowedOrigins("http://localhost:8080", "https://typingtest.up.railway.app")
-//                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-//                 .allowedHeaders("*")
-//                 .allowCredentials(true);
-//     }
-// }
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:8080", "https://typingtest.up.railway.app")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
