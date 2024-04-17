8a0bfb48a8bb1fa335f5f30afd95e3caf224ea22
Cecila <cecilachewyc@gmail.com>
2024-04-17 08:42:10 +0800

    Update environment.prod.ts

--- frontend/typing-simulator-frontend/src/environments/environment.prod.ts
+++ frontend/typing-simulator-frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: '${API_URL}',
+    apiUrl: 'backend-production-b3bc.up.railway.app',
     github_client_id: '${GITHUB_CLIENT_ID}',
     redirectUri: '${GITHUB_REDIRECT_URI}',
 };
