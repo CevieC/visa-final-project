c1de96f157ca0da7576ff3358332185a175f6547
Cecila <cecilachewyc@gmail.com>
2024-04-17 08:47:03 +0800

    Debug

--- frontend/typing-simulator-frontend/src/environments/environment.prod.ts
+++ frontend/typing-simulator-frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: 'backend-production-b3bc.up.railway.app',
+    apiUrl: 'https://backend-production-b3bc.up.railway.app',
     github_client_id: '${GITHUB_CLIENT_ID}',
     redirectUri: '${GITHUB_REDIRECT_URI}',
 };
