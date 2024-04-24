71b3b0a02df3963e84a8bcc6a4343e3eeb8cfa47
Cecila Chew <148834098+CevieC@users.noreply.github.com>
2024-04-25 07:12:36 +0800

    Update environment.prod.ts

--- frontend/typing-simulator-frontend/src/environments/environment.prod.ts
+++ frontend/typing-simulator-frontend/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
-    apiUrl: 'localhost',
-    // apiUrl: 'https://backend-production-b3bc.up.railway.app',
+    // apiUrl: 'localhost',
+    apiUrl: 'https://backend-production-b3bc.up.railway.app',
     github_client_id: '${GITHUB_CLIENT_ID}',
     redirectUri: '${GITHUB_REDIRECT_URI}',
 };
